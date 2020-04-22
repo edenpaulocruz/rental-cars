0a799b8545fc3cedbd96dcac466ddf9da0b2d6d9
Eden Paulo Cruz <edenpaulo@gmail.com>
2020-04-22 02:45:13 -0300

    Cadastrar um fabricante

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -7,4 +7,14 @@ class ManufacturersController < ApplicationController
     id = params[:id]
     @manufacturer = Manufacturer.find(id)
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(params.require(:manufacturer).permit(:name))
+    @manufacturer.save
+    redirect_to @manufacturer
+  end
 end
