157d153829b18fb9e0281562a3fb551dd91ed420
Eden Paulo Cruz <edenpaulo@gmail.com>
2020-04-24 12:56:13 -0300

    Refatoração to teste Administrador vê todas categorias de carro

--- spec/features/admin_view_car_categories_spec.rb
+++ spec/features/admin_view_car_categories_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Admin view car categories' do
+feature 'Admin view car categories and return to home page' do
   scenario 'successfully' do
     CarCategory.create!(name: 'A', daily_rate: 100)
     CarCategory.create!(name: 'B', daily_rate: 80)
@@ -12,9 +12,13 @@ feature 'Admin view car categories' do
     expect(page).to have_content(100)
     expect(page).to have_content('B')
     expect(page).to have_content(80)
+
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
   end
 
-  scenario 'and view details' do
+  scenario 'and view details, and return to car categories page' do
     CarCategory.create!(name: 'A', daily_rate: 100, car_insurance: 60, third_part_insurance: 30)
     CarCategory.create!(name: 'B', daily_rate: 80, car_insurance: 48, third_part_insurance: 24)
 
@@ -30,35 +34,17 @@ feature 'Admin view car categories' do
     expect(page).to have_content(80)
     expect(page).to have_content(48)
     expect(page).to have_content(24)
+
+    click_on 'Voltar'
+
+    expect(current_path).to eq car_categories_path
   end
 
-  scenario 'and no car_categories are created' do
+  scenario 'and no car categories created' do
     visit root_path
     click_on 'Categorias de carro'
 
     expect(page).to have_content('Nenhuma categoria de carros cadastrada')
   end
 
-  scenario 'and return to home page' do
-    CarCategory.create!(name: 'A', daily_rate: 100)
-    CarCategory.create!(name: 'B', daily_rate: 80)
-
-    visit root_path
-    click_on 'Categorias de carros'
-    click_on 'Voltar'
-
-    expect(current_path).to eq root_path
-  end
-
-  scenario 'and return to car categories page' do
-    CarCategory.create!(name: 'A', daily_rate: 100, car_insurance: 60, third_part_insurance: 30)
-    CarCategory.create!(name: 'B', daily_rate: 80, car_insurance: 48, third_part_insurance: 24)
-
-    visit root_path
-    click_on 'Ctegorias de carros'
-    click_on 'B'
-    click_on 'Voltar'
-
-    expect(current_path).to eq car_categories_path
-  end
 end
