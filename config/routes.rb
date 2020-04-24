86af0160fad32fa7b81515da8f7f94d4c63b2117
Eden Paulo Cruz <edenpaulo@gmail.com>
2020-04-23 21:22:32 -0300

    Editar um fabricante

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources 'manufacturers', only: [:index, :show, :create, :new]
+  resources 'manufacturers', only: [:index, :show, :create, :new, :edit, :update]
 end
